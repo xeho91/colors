64ef7e7dc86f584bf8a6099c48e33085471c1c85
xeho91 <xeho91@pm.me>
2021-06-13 22:49:41 +0800

    build(types): 🏷️  Add Color class to shared types

--- source/cli/types.ts
+++ source/cli/types.ts
@@ -2,6 +2,7 @@ import type { SelectValueOptions } from "https://deno.land/x/cliffy@v0.19.1/mod.
 import type { BuildCSSoptions } from "./commands/build/css.ts";
 import type { BuildOptions } from "./commands/build.ts";
 import type { DetailsArgument, DetailsOptions } from "./commands/details.ts";
+import Color from "./helpers/Color.ts";
 import type { ColorsData } from "./helpers/data.ts";
 import type { GlobalOptions } from "./mod.ts";
 
@@ -10,6 +11,7 @@ type ColorCode = "hsl" | "rgb" | "hex";
 export type {
 	BuildCSSoptions,
 	BuildOptions,
+	Color,
 	ColorCode,
 	ColorsData,
 	DetailsArgument,
